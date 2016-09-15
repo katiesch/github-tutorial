114e54df30fb7f1660388b5107a07b51d46a41ef
katiesch <kschlesinger@gmail.com>
2016-09-15 13:09:04 -0700

    added insight response

--- exercise1/exercise1.py
+++ exercise1/exercise1.py
@@ -4,3 +4,7 @@ def print_message(message):
 def print_art(image): 
 	print image
 
+def hi_fellow(message): 
+	if (message=='I am an Insight Fellow!"): 
+		print 'That's awesome! How is that random forest model going?'
+
